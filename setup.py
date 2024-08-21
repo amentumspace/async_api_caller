d672826fa67bbcca7565d92c25b8bd523fa4731a
Iwan Cornelius <iwan@amentum.com.au>
2024-08-22 09:49:07 +1000

    added rich progress bar and dependency to setup

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="async_api_caller",
-    version="0.1.0",
-    author="Amentum Scientific",
+    version="0.2.0",
+    author="https://amentum.io",
     author_email="team@amentum.space",
     description="Easily make asynchronous API web calls in Pythons",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/amentumspace/async_api_caller.git",
     packages=setuptools.find_packages(),
-    install_requires=['aiohttp', 'asyncio'],
+    install_requires=['aiohttp', 'asyncio', 'rich'],
 )
 
